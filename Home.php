fa067c26bc7fb4a17f190707444db2d277d408fb
xtiangarderepas <xtiangarderepas@gmail.com>
2019-03-25 12:42:05 -0700

    Project first commit

---
+++ Home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body{
+                 background-image: url(otirasor1.jpg);
+                 background-color: mintcream;
+            }
+            #header{
+                background-color: lightgrey;
+                color:white;
+                text-align: left;
+                font-family: courier;
+                padding: 30px;
+               
+            }
+            #nav{
+                line-height: 30px;
+                background-color: #48D1CC;
+                color: darkgrey;
+                font-size: 2em;
+                font-family: COURIER;
+                width: 80%;
+                float: top;
+                padding: 5px;
+            }
+            #section{
+                width: 350px;
+                float: left;
+                padding: 5px;
+                background-color: pink;
+            }
+        </style>
+           
+    </head>
+        
+    <body>
+        <div id="header">
+            <h1>Welcome to OTIRASOR BEACH RESORT MONITORING SYSTEM</h1>
+            <img src="otirasor1.jpg" 
+                 alt="Otirasor" with="200" height="100">
+        </div>
+        
+        <div id="nav">
+            <th>Customer
+            <th>Monitor
+            <th>Home
+        </div>
+        <div class="section">
+            
+        </div>
+    </body>
+    
+    
+</html>
